aa6f9cdd8c9201d871a27175e0199f403a7a7a8a
Jacob Walser <jwalser90@gmail.com>
2018-12-09 13:50:22 -0500

    html!

--- br-webui/test/test-query-usb.js
+++ br-webui/test/test-query-usb.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var app = express();
+var path = require("path");
 const child_process = require('child_process');
-app.use(express.static('public'));
-console.log("starting test server");
 
 var COMPANION_DIR = "../.."
+
 var server = app.listen(2770, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("App running at http://%s:%s", host, port);
-	
-	var cmd = child_process.exec('git describe --tags', function(error, stdout, stderr) {
-		console.log('Companion version: ', stdout);
-	});
-	
-	var cmd = child_process.exec('git rev-parse HEAD', function(error, stdout, stderr) {
-		console.log('Git revision: ', stdout);
-	});
 });
 
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + "/test-query-usb.html"));
+});
 
 app.get("/udevadm", function(req, res) {
     var pattern = "";
