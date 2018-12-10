7e9a3b055dc6349ed1af8654d3bf5b01aff405b9
Jacob Walser <jwalser90@gmail.com>
2018-12-09 22:16:24 -0500

    working...?

--- br-webui/test/test-query-usb.js
+++ br-webui/test/test-query-usb.js
@@ -6,22 +6,22 @@ const child_process = require('child_process');
 var COMPANION_DIR = "../.."
 
 var server = app.listen(2770, function() {
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log("App running at http://%s:%s", host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("App running at http://%s:%s", host, port);
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + "/test-query-usb.html"));
+    res.sendFile(path.join(__dirname + "/test-query-usb.html"));
 });
 
 app.get("/udevadm", function(req, res) {
     var pattern = "";
     if (req.query.pattern) {
         pattern = "--pattern=" + req.query.pattern;
-	}
-	
-	var result = child_process.execSync("python3 " + COMPANION_DIR + "/tools/query-usb.py --indent=2 " + pattern);
+    }
+    
+    var result = child_process.execSync("python3 " + COMPANION_DIR + "/tools/query-usb.py --indent=2 " + pattern);
     res.setHeader('Content-Type', 'application/json');
-	res.send(result.toString());
+    res.send(result.toString());
 });
