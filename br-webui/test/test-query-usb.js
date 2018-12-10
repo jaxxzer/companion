bf6dd78cfb3d412c126380f2dc79e7eabb5d3b48
Jacob Walser <jwalser90@gmail.com>
2018-12-09 23:38:57 -0500

    working?

--- br-webui/test/test-query-usb.js
+++ br-webui/test/test-query-usb.js
@@ -20,6 +20,7 @@ app.get("/udevadm", function(req, res) {
     if (req.query.pattern) {
         pattern = "--pattern=" + req.query.pattern;
     }
+    console.log("got request for query:" + req.query.pattern);
     
     var result = child_process.execSync("python3 " + COMPANION_DIR + "/tools/query-usb.py --indent=2 " + pattern);
     res.setHeader('Content-Type', 'application/json');
