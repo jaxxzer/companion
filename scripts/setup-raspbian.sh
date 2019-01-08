ac6bec46577f7e3d5159e5c34ee64d79cd99febc
Jacob Walser <jwalser90@gmail.com>
2019-01-08 13:19:12 -0500

    lay off the sudo

--- scripts/setup-raspbian.sh
+++ scripts/setup-raspbian.sh
@@ -5,14 +5,14 @@
 # /boot/config.txt
 
 # Disable camera LED
-sudo sed -i '\%disable_camera_led=1%d' /boot/config.txt
-sudo sed -i '$a disable_camera_led=1' /boot/config.txt
+sed -i '\%disable_camera_led=1%d' /boot/config.txt
+sed -i '$a disable_camera_led=1' /boot/config.txt
 
 # Enable RPi camera interface
-sudo sed -i '\%start_x=%d' /boot/config.txt
-sudo sed -i '\%gpu_mem=%d' /boot/config.txt
-sudo sed -i '$a start_x=1' /boot/config.txt
-sudo sed -i '$a gpu_mem=128' /boot/config.txt
+sed -i '\%start_x=%d' /boot/config.txt
+sed -i '\%gpu_mem=%d' /boot/config.txt
+sed -i '$a start_x=1' /boot/config.txt
+sed -i '$a gpu_mem=128' /boot/config.txt
 
 # source startup script
 S1="$HOME/companion/scripts/expand_fs.sh"
@@ -22,18 +22,18 @@ S2=". $HOME/companion/.companion.rc"
 # and commands are already in place
 # delete S1 if it already exists
 # insert S1 above the first uncommented exit 0 line in the file
-sudo sed -i -e "\%$S1%d" \
+sed -i -e "\%$S1%d" \
 -e "\%$S2%d" \
 -e "0,/^[^#]*exit 0/s%%$S1\n$S2\n&%" \
 /etc/rc.local
 
 #Delete ip address if present from /boot/cmdline.txt
 # e.g. sed command removes any ip address with any combination of digits [0-9] between decimal points
-sudo sed -i -e 's/\s*ip=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*//' /boot/cmdline.txt
+sed -i -e 's/\s*ip=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*//' /boot/cmdline.txt
 
 
 # remove any line containing 'enable_uart=' from /boot/config.txt
-sudo sed -i '/enable_uart=/d' /boot/config.txt
+sed -i '/enable_uart=/d' /boot/config.txt
 
 # append 'enable_uart=1' line to /boot/config.txt
 echo "enable_uart=1" | sudo tee -a /boot/config.txt
