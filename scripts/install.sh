a7f3165a808b8532ff560b8ab2fcbf60bb2f3be8
Jacob Walser <jwalser90@gmail.com>
2019-01-08 18:13:28 -0500

    wip

---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/setup.sh
+wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/setup-system-files.sh
+wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/setup-raspbian.sh
+chmod +x *.sh
+./setup.sh
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,19 +3,17 @@
 GIT_REPO=jaxxzer/companion
 GIT_BRANCH=setup
 
-
 export COMPANION_DIR=/home/pi/companion
 . $COMPANION_DIR/scripts/bash-helpers.sh
 
 # Update package lists and current packages
 export DEBIAN_FRONTEND=noninteractive
 APT_OPTIONS=-yq
-skip_step sudo apt update $APT_OPTIONS
-skip_step sudo apt upgrade $APT_OPTIONS
-
+run_step sudo apt update $APT_OPTIONS
+run_step sudo apt upgrade $APT_OPTIONS
 
 # install python and pip
-skip_step sudo apt install $APT_OPTIONS \
+run_step sudo apt install $APT_OPTIONS \
   rpi-update \
   python-dev \
   python-pip \
@@ -27,7 +25,6 @@ skip_step sudo apt install $APT_OPTIONS \
   libffi-dev \
   git \
   screen \
-  nodejs \
   npm \
   gstreamer1.0-tools \
   gstreamer1.0-plugins-base \
@@ -40,35 +37,43 @@ skip_step sudo apt install $APT_OPTIONS \
   v4l-utils \
   || error "failed apt install dependencies"
 
-# browser based terminal
-skip_step sudo npm install tty.js -g || error "failed npm install dependencies"
+run_step sh -c "wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
 
-skip_step sudo pip install \
+# to use nvm
+run_step source ~/.profile
+
+run_step nvm 11.6.0
+
+# browser based terminal
+run_step sudo npm install tty.js -g || error "failed npm install dependencies"
+
+run_step sudo pip install \
   future \
   pynmea2 \
   grequests \
   bluerobotics-ping \
   || error "failed pip install dependencies"
 
-skip_step sudo pip3 install future || error "failed pip3 install dependencies"
+run_step sudo pip3 install future || error "failed pip3 install dependencies"
 
 # clone bluerobotics companion repository
-skip_step git clone --depth 1 -b $GIT_BRANCH https://github.com/$GIT_REPO $COMPANION_DIR || error "failed git clone"
+run_step git clone --depth 1 -b $GIT_BRANCH https://github.com/$GIT_REPO $COMPANION_DIR || error "failed git clone"
 
-skip_step cd $COMPANION_DIR
+run_step cd $COMPANION_DIR
 
-skip_step git submodule update --init --recursive || "error failed submodule update"
+run_step git submodule update --init --recursive || "error failed submodule update"
 
-skip_step cd $COMPANION_DIR/submodules/mavlink/pymavlink
-skip_step python3 setup.py build || "error failed "
-skip_step sudo python3 setup.py install || "error failed "
+run_step cd $COMPANION_DIR/submodules/mavlink/pymavlink
+run_step python3 setup.py build || "error failed "
+run_step sudo python3 setup.py install || "error failed "
 
-skip_step cd $COMPANION_DIR/submodules/MAVProxy
-skip_step python setup.py build
-skip_step sudo python setup.py install
+run_step cd $COMPANION_DIR/submodules/MAVProxy
+run_step python setup.py build
+run_step sudo python setup.py install
 
-skip_step cd $COMPANION_DIR/br-webui
-skip_step npm install
+run_step cd $COMPANION_DIR/br-webui
+run_step export JOBS=4
+run_step npm install
 
 run_step $COMPANION_DIR/scripts/setup-raspbian.sh
 run_step $COMPANION_DIR/scripts/setup-system-files.sh
