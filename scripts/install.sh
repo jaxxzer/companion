b29c11f6b3d67bb7860daa1027d3f1c6e005c214
Jacob Walser <jwalser90@gmail.com>
2019-01-10 11:11:08 -0500

    wip

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,10 @@ wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/install-d
 wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/setup-system-files.sh
 wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/setup-raspbian.sh
 wget https://raw.githubusercontent.com/jaxxzer/companion/setup/scripts/bash-helpers.sh
+ls
 chmod +x *.sh
 ./install-dependencies.sh && \
 ./setup-raspbian.sh && \
-./bash-helpers.sh
+./setup-system-files.sh
 popd
 rm -rf tmp
