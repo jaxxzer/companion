f3078f93819820da994880ffaaf6aabbe61e154c
Jacob Walser <jwalser90@gmail.com>
2019-01-10 11:12:10 -0500

    wip

--- scripts/bash-helpers.sh
+++ scripts/bash-helpers.sh
@@ -7,13 +7,13 @@ error() {
 
 run_step() {
   echo -e ""
-  echo -e ""
   echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   echo -e "RUN STEP: $@"
   echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
   "$@" || error "failed RUN STEP $@ with code $?"
   echo -e ""
   echo -e ":) completed RUN STEP $@ with code $?"
+  echo -e ""
 }
 
 skip_step() {
