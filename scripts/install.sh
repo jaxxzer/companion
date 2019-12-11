ee388cd9fb6c00df001cb3c14f86d7d7fa80b617
Jacob Walser <jwalser90@gmail.com>
2019-12-11 16:52:04 -0500

    scripts: add installation/setup scripts

---
+++ scripts/install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+export COMPANION_DIR=$HOME/companion
+
+GIT_USER=jaxxzer
+GIT_BRANCH=setup-clean
+BASE_URL=https://raw.githubusercontent.com/$GIT_USER/companion/$GIT_BRANCH/scripts
+
+mkdir tmp || { echo "failed to make temporary directory" >&2 && exit 1; }
+pushd tmp
+
+wget $BASE_URL/bash-helpers.sh || { echo "failed to download $BASE_URL/bash-helpers.sh" >&2 && exit 1; }
+
+# source helpers
+. ./bash-helpers.sh
+
+# get scripts
+run_step wget $BASE_URL/install-dependencies.sh
+run_step wget $BASE_URL/setup-system-files.sh
+run_step wget $BASE_URL/setup-raspbian.sh
+
+run_step chmod +x *.sh
+
+# install dependencies from apt, pip etc
+run_step ./install-dependencies.sh
+
+run_step pwd
+run_step ls
+
+# setup raspbian/raspberry-pi specific files
+run_step ./setup-raspbian.sh
+
+# setup system and user configuration files
+run_step ./setup-system-files.sh
+
+# clean up, remove files
+popd
+rm -rf tmp
---
+++ scripts/setup-raspbian.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# setup raspbian / raspberry pi configuration files
+# /boot/cmdline.txt and
+# /boot/config.txt
+
+. $COMPANION_DIR/scripts/bash-helpers.sh
+
+# Disable camera LED
+run_step sudo sed -i '\%disable_camera_led=1%d' /boot/config.txt
+run_step sudo sed -i '$a disable_camera_led=1' /boot/config.txt
+
+# Enable RPi camera interface
+run_step sudo sed -i '\%start_x=%d' /boot/config.txt
+run_step sudo sed -i '\%gpu_mem=%d' /boot/config.txt
+run_step sudo sed -i '$a start_x=1' /boot/config.txt
+run_step sudo sed -i '$a gpu_mem=128' /boot/config.txt
+
+#Delete ip address if present from /boot/cmdline.txt
+# e.g. sed command removes any ip address with any combination of digits [0-9] between decimal points
+sudo sed -i -e 's/\s*ip=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*//' /uboot/cmdline.txt || sudo sed -i -e 's/\s*ip=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*//' /boot/cmdline.txt
+
+# remove any line containing 'enable_uart=' from /boot/config.txt
+run_step sudo sed -i '/enable_uart=/d' /boot/config.txt
+
+# append 'enable_uart=1' line to /boot/config.txt
+run_step echo "enable_uart=1" | sudo tee -a /boot/config.txt
+
+# run rpi update
+# run_step sudo rpi-update
